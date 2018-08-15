700073da51a6abb486a0584101f3b0428cee6206
J Fern <jfern20@gmail.com>
2018-08-15 03:02:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,9 @@ def prime(integer)
  if integer < 2 
    return false 
 end    
- (2..(integer/2.to_f).ceil)
+ (2..(integer/2.to_f).ceil).each do |x|
+   if integer % x == 0 
+     return false 
    
 
 end 
