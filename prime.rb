6c3c2399795aeedd72283cab04af018b61dc3380
J Fern <jfern20@gmail.com>
2018-08-15 02:11:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
 def prime(integer)
  integer.each do |num|
    i = 2 
+   
      while  i < num 
-        if num % i == 0
-     true 
-   else 
-     false 
+       if num % i == 0
+   
    end 
 end 
